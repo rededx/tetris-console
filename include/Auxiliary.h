baf2a895c164196d9d1d3b3a907b1e3a4e5283a1
rededx <rededrx@gmail.com>
2021-03-18 15:36:54 +0200

    Create auxiliary structure and enum

---
+++ include/Auxiliary.h
@@ -0,0 +1,26 @@
+#pragma once
+
+enum class Keyboard : int {
+  kArrow = 224,
+  kArrowUp = 72,
+  kArrowLeft = 75,
+  kArrowRight = 77,
+  kArrowDown = 80,
+  kSpace = 32,
+  kEscape = 27
+};
+
+enum class Tetromino : int {
+	I = 0,
+	O,
+	T,
+	J,
+	L,
+	S,
+	Z
+};
+
+struct Vec2 {
+  int x;
+  int y;
+};
