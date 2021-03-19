dabe4bf57dc9bb5602329c49540c9868b7d4aea4
rededx <rededrx@gmail.com>
2021-03-19 13:21:43 +0200

    Documenting code

--- tetris/include/Tetris.h
+++ tetris/include/Tetris.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <Windows.h>
+
 #include <array>
 #include <cmath>
 #include <memory>
 #include <random>
-#include <utility>
 #include <string>
+#include <utility>
 
 #include "Auxiliary.h"
 #include "BaseApp.h"
@@ -14,33 +15,98 @@
 using Mat2 = std::array<Vec2, 2>;
 using Mat4x2 = std::array<Vec2, 4>;
 
+/**
+ * @brief Class that implements Tetris logic.
+ *
+ * Inherits a render class that calls overridden program logic functions.
+ */
 class Tetris : public BaseApp {
  public:
+  /**
+   * @brief Construct a new Tetris object.
+   *
+   * The constructor sets the initial coordinates for drawing the game
+   * interface.
+   */
   Tetris();
 
+  /**
+   * @brief Event handling method.
+   *
+   * Left / Right Arrows to move the Tetrimino left / right.
+   * The down arrow accelerates the movement of the figure until the next
+   * tetrimino appears.
+   * The space bar wraps around the tetrimino.
+   *
+   * @param btnCode Accepts the pressed button.
+   */
   void KeyPressed(int btnCode) override;
+
+  /**
+   * @brief Determining the coordinates of the tetrimino and sending them for
+   * drawing.
+   *
+   * @param deltaTime Time difference between the previous iteration and this
+   * one, in seconds.
+   */
   void UpdateF(float deltaTime) override;
 
+  /**
+   * @brief Get the Next random Tetromino number
+   *
+   * @return Tetromino number next tetromino
+   */
   Tetromino GetNextTetromino();
+
+  /**
+   * @brief Determination of local coordinates of tetromino
+   *
+   * Determines the coordinates of a tetromino from a matrix in the local
+   * coordinate system.
+   *
+   * @param tetrominoNum A tetromino number that identifies a tetromino from an
+   * array of figures.
+   * @return Mat4x2 Local coordinates of tetromino.
+   */
   Mat4x2 CalculateCoordinatesTetromino(Tetromino tetrominoNum);
-  Mat4x2 Rotate(Mat4x2&);
-  bool CheckNewPosition(Mat4x2& objectj, Vec2&& vectorMove);
-  void LineFill—heck();
+
+  /**
+   * @brief Rotate tetromino/
+   *
+   * @param object Curent tetromino.
+   * @return Mat4x2 New tetromino.
+   */
+  Mat4x2 Rotate(Mat4x2& object);
+
+  /**
+   * @brief Checking a new position.
+   *
+   * Checking if the new position is free and if we have gone out of the play
+   * area.
+   *
+   * @param object Curent tetromino.
+   * @param vectorMove The vector of the direction of movement of the object.
+   * @return true New position fits.
+   * @return false New position does not fit.
+   */
+  bool CheckNewPosition(Mat4x2& object, Vec2&& vectorMove);
+
+  /**
+   * @brief Line fill check.
+   *
+   * Checking the filling of the line, clear if the line is full and changing
+   * the score readings.
+   */
+  void LineFillCheck();
 
  private:
-  float mDelay = 0.5f;
-  float mTime = 0.0f;
-
-  Vec2 mGameFieldLeftUp;
-  Vec2 mGameFieldRightDown;
-
-  int mWindowWidth, mWindowHeight;
-  bool mKeyboradArrowFlag = false;
-
-  Vec2 mSpawnPosition;
-  Vec2 mSpawnPositionPreview;
-  Vec2 mSpawnPositionScore;
-
+  /**
+   * @brief
+   *
+   * Tetriminos are defined in a 4 by 4 matrix. The matrix is numbered for each
+   * cell starting at 0. By drawing a figure in this matrix, we get the numbers
+   * that correspond to this figure.
+   */
   std::array<std::array<int, 4>, 7> mTetrominoFigures = {{
       {1, 5, 9, 13},  // I
       {1, 2, 5, 6},   // O
@@ -55,11 +121,34 @@ class Tetris : public BaseApp {
   Mat4x2 mTetrominoOld;
   Mat4x2 mTetrominoNext;
 
+  long mScore = 0;
+
+  Vec2 mGameFieldLeftUp;
+  Vec2 mGameFieldRightDown;
+
+  Vec2 mSpawnPosition;
+  Vec2 mSpawnPositionPreview;
+  Vec2 mSpawnPositionScore;
+
+  /**
+   * @brief Time delay through which the tetromino moves downward.
+   */
+  float mDelay = 0.5f;
+
+  float mTime = 0.0f;
+
   Tetromino mTetrominoNum = Tetromino::kUnknown;
   Tetromino mTetrominoNextNum = Tetromino::kUnknown;
 
-  // true = vertical
+  int mWindowWidth, mWindowHeight;
+
+  /**
+   * @brief Determines the horizontal/vertical position of the tetromino.
+   *
+   * false The horizontal position of the tetromino.
+   * true The vertical position of the tetromino.
+   */
   bool mStateTetromino = true;
 
-  long mScore = 0;
+  bool mKeyboradArrowFlag = false;
 };
